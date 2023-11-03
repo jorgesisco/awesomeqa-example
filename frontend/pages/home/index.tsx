8bb5fea2fb15389a92f46a520cf6c44dbbd5b459
Jorge Sisco <jorgesisco17@gmail.com>
2023-11-03 23:20:47 +0100

    Adding buttons to the page home

--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -2,8 +2,11 @@ import * as React from "react";
 import { NextPage } from "next";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import { Button } from "@mui/material";
-
+import Link from 'next/link';
+import CustomButton from '../../components/PageButton';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined'; // Sample icon
+import SupportAgentOutlinedIcon from '@mui/icons-material/SupportAgentOutlined'; // Sample icon
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined'; // Sample icon
 const Home: NextPage = () => {
 
   const handleClick = async () => {
@@ -11,24 +14,26 @@ const Home: NextPage = () => {
   };
 
   return (
-    <>
-      <Box sx={{ flexGrow: 1, mt: 15, mb: 15 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleClick}
-                sx={{ width: "50%", height: "4rem", fontSize: "1.2rem" }}
-              >
-                Some other Button
-              </Button>
-            </Box>
-          </Grid>
+    <Box sx={{ flexGrow: 1, mt: 15, mb: 15, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "50vh" }}>
+      <Grid container spacing={-60} justifyContent="center" alignItems="center">
+      
+        <Grid item xs={12} md={4}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Link href="/knowledge"><CustomButton Icon={ArticleOutlinedIcon} label="Knowledge Base"/></Link>
+          </Box>
         </Grid>
-      </Box>
-    </>
+        <Grid item xs={12} md={4}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Link href="/tickets"><CustomButton Icon={SupportAgentOutlinedIcon} label="Tickets" /></Link>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Link href="/faq"><CustomButton Icon={LightbulbOutlinedIcon} label="FaQ Insights"/></Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
