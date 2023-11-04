2c351e408a2782401eb3d6b7e80fd69e0fccd708
Jorge Sisco <jorgesisco17@gmail.com>
2023-11-04 21:20:50 +0100

    Add CORS middleware to FastAPI app for localhost:3000

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,20 @@
 """Main module for FastAPI application."""""
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 # Initialize FastAPI application
 app = FastAPI()
+
+# Register CORS middleware to allow specific origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],  # The methods you want to allow
+    allow_headers=["*"],  # The headers you want to allow
+)
 
 # Register router with views
 app.include_router(router)
