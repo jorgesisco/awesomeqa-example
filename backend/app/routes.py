c8d566f9ef5b395a47986dae5c7c6935687366e8
Jorge Sisco <jorgesisco17@gmail.com>
2023-11-04 21:26:16 +0100

    Updated ticket retrieval to include messages and adjusted count method

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,9 @@
+"""Routes module."""
 from app.config import ticket_repository
 from app.models import Ticket, Pagination
 from app.repositories.ticket_repository import TicketRepository
 from fastapi import APIRouter, Depends, HTTPException, Query
 
-from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -22,8 +22,9 @@ async def get_tickets(
 ):
     """Get tickets with pagination."""
     offset = (page - 1) * limit
-    total_tickets = ticket_repository.count_tickets()
-    tickets_data = ticket_repository.get_tickets(limit=limit, offset=offset)
+    total_tickets = ticket_repository.count_items(field="tickets")
+
+    tickets_data = ticket_repository.get_tickets_with_messages(limit=limit, offset=offset)
     tickets = [Ticket(**ticket) for ticket in tickets_data]
 
     # Check if the current page is out of range
