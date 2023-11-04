44460eada5227396c107ba0ac2f1cb0bae323386
Jorge Sisco <jorgesisco17@gmail.com>
2023-11-04 15:51:06 +0100

    Refactored /tickets endpoint to support pagination and updated health check response
    - Updated the /tickets GET endpoint to handle pagination, allowing clients to request tickets in a paginated format.
    - Enhanced the health check endpoint to return a JSON response.
    - Integrated the Ticket and Pagination models to structure the API response data.

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,8 @@
 from app.config import ticket_repository
+from app.models import Ticket, Pagination
 from app.repositories.ticket_repository import TicketRepository
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Query
+
 from fastapi.responses import JSONResponse
 
 router = APIRouter()
@@ -9,15 +11,28 @@ router = APIRouter()
 @router.get("/healthz")
 async def root():
     """Health check endpoint."""
-    return "OK"
+    return {"status": "OK"}
 
 
 @router.get("/tickets")
 async def get_tickets(
-        limit: int = 20,
+        page: int = Query(1, gt=0, description="Page number starting from 1"),
+        limit: int = Query(20, gt=0, description="Number of tickets per page"),
         ticket_repository: TicketRepository = Depends(lambda: ticket_repository),
 ):
-    """Get tickets endpoint."""
-    tickets = ticket_repository.get_tickets(limit)
-    return JSONResponse(tickets, status_code=200)
+    """Get tickets with pagination."""
+    offset = (page - 1) * limit
+    total_tickets = ticket_repository.count_tickets()  # Assuming this method exists
+    tickets_data = ticket_repository.get_tickets(limit=limit, offset=offset)
+    tickets = [Ticket(**ticket) for ticket in tickets_data]
 
+    # Check if the current page is out of range
+    if not tickets and page != 1:
+        raise HTTPException(status_code=404, detail="Page not found")
+
+    return Pagination(
+        total=total_tickets,
+        page=page,
+        limit=limit,
+        data=tickets
+    )
