c70e0bab72e2b87eee10be04fe1b0eaeb1fdc365
Jorge Sisco <jorgesisco17@gmail.com>
2023-11-04 21:33:00 +0100

    Adding TicketsTable component to the Tickets page

--- frontend/pages/tickets/index.tsx
+++ frontend/pages/tickets/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { NextPage } from "next";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import TicketsTable from "../../components/ticketsTable";
 
 
 const tickets: NextPage = () => {
@@ -9,16 +10,14 @@ const tickets: NextPage = () => {
   const handleClick = async () => {
     console.log("clicked");
   };
-
+  
   return (
     <>
       <Box sx={{ flexGrow: 1, mt: 15, mb: 15 }}>
-        <Grid container spacing={2}>
+        <Grid container spacing={3} justifyContent="center">
           <Grid item xs={12}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              
-               Implement Tickets page here!
-             
+            <Box sx={{ display: "flex", justifyContent: "center", height: 550, maxWidth: '100%' }}>
+            <TicketsTable />
             </Box>
           </Grid>
         </Grid>
