297f3c2dc02b2dcaddb87424ffe9268ba7dc8b99
Jorge Sisco <jorgesisco17@gmail.com>
2023-11-04 15:58:45 +0100

    Added docstrings to current instance method in TicketRepository classes

--- backend/app/repositories/ticket_repository.py
+++ backend/app/repositories/ticket_repository.py
@@ -5,15 +5,18 @@ from typing import Optional
 
 class TicketRepository:
     """
-     A simple repository to fetch tickets and messages
-     from a JSON file.
+    A simple repository to fetch tickets and messages
+    from a JSON file.
      """
     def __init__(self, filepath: str | Path):
         with open(filepath) as json_file:
             self.data = json.load(json_file)
 
     def get_tickets(self, offset: int, limit: Optional[int] = None) -> list[dict]:
-
+        """
+        Return a slice of tickets based on the offset and limit.
+        or all tickets if limit is not specified.
+        """
         # If the offset is greater than the number of tickets, return an empty list
         if offset >= len(self.data["tickets"]):
             return []
@@ -27,9 +30,14 @@ class TicketRepository:
         return self.data["tickets"][offset:offset + limit]
 
     def count_tickets(self) -> int:
+        """Return the total number of tickets."""
         return len(self.data["tickets"])
 
     def get_messages(self, limit: Optional[int] = None) -> list[dict]:
+        """
+        Return default limit set in route or total
+        messages if limit is None.
+        """
         if limit is None:
             return self.data["messages"]
         else:
