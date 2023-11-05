d82438a836bb0e96e556b59e881166e1c0012db8
Jorge Sisco <jorgesisco17@gmail.com>
2023-11-05 11:24:04 +0100

    Removed  comment in fetchTickets function

--- frontend/hooks/userTickets.tsx
+++ frontend/hooks/userTickets.tsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 export async function fetchTickets(url) {
   try {
     const response = await axios.get(url);
-    // Transform the data here if needed
     return response.data.data;
   } catch (error) {
     // Handle or throw the error
