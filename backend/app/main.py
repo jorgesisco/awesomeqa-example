24171b9db97c61e104b70c5d7fc622ecec1d8911
Jorge Sisco <jorgesisco17@gmail.com>
2023-11-05 12:05:27 +0100

    Integrated rate limiting with SlowAPI and handle RateLimitExceeded exceptions
    - Configured SlowAPI limiter and added to FastAPI state
    - Registered global exception handler for RateLimitExceeded
    - Updated CORS middleware setup
    - Refactored main module structure for improved rate limit handling
    - Refactored module by following pylint's guidance

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,25 @@
 """Main module for FastAPI application."""""
+# pylint: disable=import-error
 import uvicorn
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+
 from app.routes import router
+from app.utils.rate_limit import limiter
+
 
 # Initialize FastAPI application
 app = FastAPI()
+
+
+# Register the rate limit exceeded handler
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 
 # Register CORS middleware to allow specific origins
 app.add_middleware(
