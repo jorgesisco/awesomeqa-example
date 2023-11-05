b9e0f7101532ea0c032eccc7c41dc50e957c88b6
Jorge Sisco <jorgesisco17@gmail.com>
2023-11-05 11:23:10 +0100

    Refactored pagination to support different models and encapsulated paginated responses

--- backend/app/utils/pagination.py
+++ backend/app/utils/pagination.py
@@ -6,7 +6,7 @@ and fetching paginated ticket data from the repository.
 """
 
 from http.client import HTTPException
-from app.models import TicketWithMessage, Ticket
+from app.models import TicketWithMessage, Ticket, TicketWithMessagePagination, TicketPagination
 from app.repositories.ticket_repository import TicketRepository
 from fastapi import HTTPException, Query
 
@@ -30,18 +30,20 @@ async def get_paginated_tickets(ticket_repository: TicketRepository, page: int, 
     if with_messages:
         tickets_data = ticket_repository.get_tickets_with_messages(limit=limit, offset=offset)
         ticket_model = TicketWithMessage
+        pagination = TicketWithMessagePagination
     else:
         tickets_data = ticket_repository.get_tickets(limit=limit, offset=offset)
         ticket_model = Ticket
+        pagination = TicketPagination
 
     tickets = [ticket_model(**ticket) for ticket in tickets_data]
 
     if not tickets and page != 1:
         raise HTTPException(status_code=404, detail="Page not found")
 
-    return {
-        "total": total_tickets,
-        "page": page,
-        "limit": limit,
-        "data": tickets
-    }
+    return pagination(
+        total=total_tickets,
+        page=page,
+        limit=limit,
+        data=tickets
+    )
